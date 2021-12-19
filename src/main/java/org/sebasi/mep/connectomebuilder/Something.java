5909f7c2f6eacfeb9c99b4ff0a33a69bab2ac4f4
ewadsworth <ewadsworth@marqeta.com>
2021-12-19 10:10:40 -0500

    Setup project

---
+++ src/main/java/org/sebasi/mep/connectomebuilder/Something.java
@@ -0,0 +1,12 @@
+package org.sebasi.mep.connectomebuilder;
+
+public class Something {
+
+    public static void main(String... args) {
+        System.out.println("Hello world");
+    }
+
+    static String showSomething() {
+        return "5";
+    }
+}
---
+++ src/test/java/org/sebasi/mep/connectomebuilder/SomethingTest.java
@@ -0,0 +1,11 @@
+package org.sebasi.mep.connectomebuilder;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SomethingTest {
+    @Test
+    public void testSomething() {
+        Assertions.assertEquals("5", Something.showSomething());
+    }
+}
